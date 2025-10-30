78a1b0b3b6bb248a53438ee5fe6513923c242824
Kaiky1007 <kaikyxcunha@gmail.com>
2025-10-30 19:43:22 -0300

    Define taxas de câmbio e adiciona listener de clique

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const converterBtn = document.getElementById('converter');
     const resultadoP = document.getElementById('resultado');
 
+    const taxas = {
+        'BRL': 1.0,
+        'USD': 0.18587, 
+        'EUR': 0.16051
+    };
+
+    converterBtn.addEventListener('click', () => {
+
+        console.log('Botão clicado!'); 
+    });
 });
