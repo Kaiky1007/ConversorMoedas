fd76ca7d3152f56359549ea692aec89045a32cf9
Kaiky1007 <kaikyxcunha@gmail.com>
2025-10-30 19:42:03 -0300

    Seleciona elementos e estrutura inicial

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const valorInput = document.getElementById('valor');
+    const deSelect = document.getElementById('de');
+    const paraSelect = document.getElementById('para');
+    const converterBtn = document.getElementById('converter');
+    const resultadoP = document.getElementById('resultado');
+
+});
